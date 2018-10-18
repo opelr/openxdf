b5276307ad7533d383cb6fd75172bf41ad8a3fb2
Ryan Opel <ryan.a.opel@gmail.com>
2018-10-18 13:58:11 -0700

    Built v0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='OpenXDF',
-    version='0.1.0',
+    version='0.2.0',
     author='Ryan Opel',
     author_email='ryan.a.opel@gmail.com',
     packages=['openxdf'],
