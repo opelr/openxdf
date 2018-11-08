7c42f2fc5d84da216404664725b5c91f79827356
Ryan Opel <ryan.a.opel@gmail.com>
2018-11-08 11:03:01 -0800

    Updated unittests for helper and xdf scripts

--- tests/test_xdf_class.py
+++ tests/test_xdf_class.py
@@ -1,9 +1,6 @@
-"""[summary]
-"""
 
 from .context import openxdf
 import unittest
-from collections import OrderedDict
 
 
 class XDF_Test(unittest.TestCase):
@@ -11,10 +8,32 @@ class XDF_Test(unittest.TestCase):
 
     def __init__(self, methodName="runTest"):
         super().__init__(methodName)
-        self.fpath = "tests/data/RBD_Scored.xdf"
+        self.fpath = "tests/data/test.xdf"
+        self.xdf = openxdf.OpenXDF(self.fpath)
 
     def test_read_data(self):
-        xdf = openxdf.helpers.read_data(self.fpath)
-        assert type(xdf) is OrderedDict
-    
+        assert type(self.xdf) is openxdf.xdf.OpenXDF
+        assert type(self.xdf._data) is dict
 
+    def test_id(self):
+        xdf_id = self.xdf.id
+        assert type(xdf_id) is str
+
+    def test_start_time(self):
+        start_time = self.xdf.start_time
+        # print(start_time)
+
+    def test_headers(self):
+        header = self.xdf.header
+        assert type(header) is dict
+
+        keys = ["id", "epoch_length", "frame_length", "endian", "file"]
+        assert all([i in header.keys() for i in keys])
+
+    def test_sources(self):
+        source = self.xdf.sources
+        assert type(source) is list
+        assert type(source[0]) is dict
+
+    def test_scoring(self):
+        scoring = self.xdf.scoring
