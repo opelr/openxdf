baef693e2bcb0450ee9091d9b3205b491c5fb1ca
Ryan Opel <ryan.a.opel@gmail.com>
2018-11-08 16:06:57 -0800

    Tests for xdf module

---
+++ tests/test_xdf.py
@@ -0,0 +1,40 @@
+
+from .context import openxdf
+import unittest
+
+
+class XDF_Test(unittest.TestCase):
+    """[summary]"""
+
+    def __init__(self, methodName="runTest"):
+        super().__init__(methodName)
+        self.xdf_path = "tests/data/test.xdf"
+        self.xdf = openxdf.OpenXDF(self.xdf_path)
+
+    def test_read_data(self):
+        assert type(self.xdf) is openxdf.xdf.OpenXDF
+        assert type(self.xdf._data) is dict
+
+    def test_id(self):
+        xdf_id = self.xdf.id
+        assert type(xdf_id) is str
+
+    def test_start_time(self):
+        start_time = self.xdf.start_time
+        # print(start_time)
+
+    def test_headers(self):
+        header = self.xdf.header
+        assert type(header) is dict
+
+        keys = ["ID", "EpochLength", "FrameLength", "Endian", "File"]
+        assert all([i in header.keys() for i in keys])
+
+    def test_sources(self):
+        source = self.xdf.sources
+        assert type(source) is list
+        assert type(source[0]) is dict
+
+    def test_scoring(self):
+        scoring = self.xdf.scoring
+        
