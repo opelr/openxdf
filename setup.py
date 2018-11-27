d5bf3ddf72982dc74577ac75181f1cd272f3e1bb
Ryan Opel <ryan.a.opel@gmail.com>
2018-11-27 10:52:00 -0800

    Updated version information for new package dist

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name='OpenXDF',
-    version='0.3.0',
-    author='Ryan Opel',
-    author_email='ryan.a.opel@gmail.com',
-    packages=['openxdf'],
-    license='LICENSE',
-    description='OpenXDF is a Python module built for interacting with Open eXchange Data Format files.',
-    long_description=open('README.md').read(),
-    install_requires=[
-        "pandas >= 0.23.0",
-        "xmltodict == 0.11.0"
-    ],
+    name="OpenXDF",
+    version="0.4.0",
+    author="Ryan Opel",
+    author_email="ryan.a.opel@gmail.com",
+    url="https://github.com/opelr/openxdf",
+    packages=["openxdf"],
+    license="LICENSE",
+    description="OpenXDF is a Python module built for interacting with Open eXchange Data Format files.",
+    long_description=open("README.md").read(),
+    install_requires=["pandas >= 0.23.0", "xmltodict == 0.11.0"],
 )
